941c6a358e6d481e40e629662ed51c5e5a7d1892
Yogie-Anugrah <yogieanugrahramadhan@gmail.com>
2025-03-12 13:37:15 +0700

    update dashboard legacy

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -28,7 +28,7 @@ class Dashboard extends Component
     public function generateRandomStats()
     {
         $this->stats = [
-            'vehicles' => rand(800, 2500), // Total kendaraan dalam rentang masuk akal
+            'vehicles' => rand(400, 2500), // Total kendaraan dalam rentang masuk akal
             'congestion' => rand(10, 80), // Kemacetan dalam persentase
             'average_delay' => rand(5, 60) // Keterlambatan dalam menit
         ];
