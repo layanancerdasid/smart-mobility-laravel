ab4c3552d9fb225f2e8d8abeb683d8745991bcbd
Yogie-Anugrah <yogieanugrahramadhan@gmail.com>
2025-03-25 16:33:27 +0700

    update chart every 1 minute

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -4,8 +4,8 @@
     <div class="text-center mb-4 mt-4">
         <h4 class="fw-bold text-white py-2 rounded" style="background-color: #21294A">Jumlah Total Kendaraan</h4>
     </div>
-    <livewire:perbandingan-kendaraan />
-    <livewire:traffic-chart />
+    <livewire:perbandingan-kendaraan wire:poll.60s />
+    <livewire:traffic-chart wire:poll.60s />
     <div class="text-center mb-4 mt-4">
         <h4 class="fw-bold text-white py-2 rounded" style="background-color: #21294A">Jumlah Total Kendaraan</h4>
     </div>
